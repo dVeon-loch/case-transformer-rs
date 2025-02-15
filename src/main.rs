674f427e92765d58ae0bb9a7b4e761864fc47b61
dVeon-loch <dveonloch@protonmail.com>
2025-02-15 12:23:39 +0200

    Some minor refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 //! Main entry point into the Case Transformer server application
 use actix_cors::Cors;
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer};
 use case_transformer_rs::alive;
+
+const SERVER_PORT: u16 = 5000;
+
+const LOG_LEVEL: &'static str = "debug";
 
 #[actix_web::main]
 async fn main() -> eyre::Result<()> {
     init_server().await
 }
 
-pub async fn init_server() -> eyre::Result<()> {
-    std::env::set_var("RUST_LOG", "debug");
+/// Sets up the logging for the application
+pub fn init_logs() {
+    std::env::set_var("RUST_LOG", LOG_LEVEL);
     env_logger::init();
+}
 
+/// Contains the logic required to initiate the actix webserver
+/// and set up the endpoints
+pub async fn init_server() -> eyre::Result<()> {
     let server = HttpServer::new(move || {
         App::new()
             .wrap(
@@ -23,7 +32,7 @@ pub async fn init_server() -> eyre::Result<()> {
             )
             .service(web::scope("/api/v1").service(alive))
     })
-    .bind(("127.0.0.1", 5000))?
+    .bind(("127.0.0.1", SERVER_PORT))?
     .run();
 
     server.await?;
