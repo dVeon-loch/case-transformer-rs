4b6fc552fc1069dfcd9fdd157dc5c3c3f86f8157
dVeon-loch <dveonloch@protonmail.com>
2025-02-16 19:57:31 +0200

    Add multi-paragraph tests. Tests now failing as I am transforming all text nodes even if not inside <p> tags

--- tests/transform.rs
+++ tests/transform.rs
@@ -94,4 +94,100 @@ mod tests {
 
         assert_eq!(body_str, r#"<p>hello world</p>"#);
     }
+
+    #[actix_web::test]
+    async fn transform_upper_multi_paragraph() {
+        let app = test::init_service(
+            App::new()
+                .wrap(
+                    Cors::default()
+                        .allow_any_header()
+                        .allow_any_method()
+                        .allow_any_origin()
+                        .max_age(3600), // Cache preflight request for 1 hour
+                )
+                .service(transform),
+        )
+        .await;
+
+        let request_payload = TransformRequest::new(
+            TransformMethod::UPPER,
+            r#"<div><p>First paragraph</p><span>Not a paragraph</span><p>Second
+paragraph</p></div>"#
+                .to_string(),
+        );
+
+        let req = test::TestRequest::get()
+            .uri("/transform")
+            .set_json(request_payload)
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+
+        assert!(
+            resp.status().is_success(),
+            "Request failed with status: {}. Response body: {:?}",
+            resp.status(),
+            test::read_body(resp).await
+        );
+
+        assert!(resp.status().is_success());
+
+        let content_type = resp.headers().get("Content-Type").unwrap();
+        assert_eq!(content_type, "text/plain; charset=utf-8");
+
+        let body = test::read_body(resp).await;
+
+        let body_str = String::from_utf8(body.to_vec()).unwrap();
+
+        assert_eq!(body_str, r#"<div><p>FIRST PARAGRAPH</p><span>Not a paragraph</span><p>SECOND
+PARAGRAPH</p></div>"#);
+    }
+
+    #[actix_web::test]
+    async fn transform_lower_multi_paragraph() {
+        let app = test::init_service(
+            App::new()
+                .wrap(
+                    Cors::default()
+                        .allow_any_header()
+                        .allow_any_method()
+                        .allow_any_origin()
+                        .max_age(3600), // Cache preflight request for 1 hour
+                )
+                .service(transform),
+        )
+        .await;
+
+        let request_payload = TransformRequest::new(
+            TransformMethod::LOWER,
+            r#"<div><p>First paragraph</p><span>Not a paragraph</span><p>Second
+paragraph</p></div>"#
+                .to_string(),
+        );
+
+        let req = test::TestRequest::get()
+            .uri("/transform")
+            .set_json(request_payload)
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+
+        assert!(
+            resp.status().is_success(),
+            "Request failed with status: {}. Response body: {:?}",
+            resp.status(),
+            test::read_body(resp).await
+        );
+
+        assert!(resp.status().is_success());
+
+        let content_type = resp.headers().get("Content-Type").unwrap();
+        assert_eq!(content_type, "text/plain; charset=utf-8");
+
+        let body = test::read_body(resp).await;
+
+        let body_str = String::from_utf8(body.to_vec()).unwrap();
+
+        assert_eq!(body_str, r#"<div><p>FIRST PARAGRAPH</p><span>Not a paragraph</span><p>SECOND
+PARAGRAPH</p></div>"#);
+    }
 }
